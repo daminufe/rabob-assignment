b92e42b7367ba7d529729275908c8a0c4797e68a
David Ferreira <daminufe.ti@gmail.com>
2016-02-03 09:31:07 +0100

    Initial commit

---
+++ src/scripts/controllers/issues.ctrl.js
@@ -0,0 +1,9 @@
+(function (angular) {
+    'use strict';
+
+    angular.module('RabobankAssignment')
+    .controller('IssuesController', [function() {
+        var vm = this;
+        vm.page = 'Issues';
+    }]);
+}(angular));
---
+++ src/scripts/controllers/records.ctrl.js
@@ -0,0 +1,9 @@
+(function (angular) {
+    'use strict';
+
+    angular.module('RabobankAssignment')
+    .controller('RecordsController', [function() {
+        var vm = this;
+        vm.page = 'Records';
+    }]);
+}(angular));
